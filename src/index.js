40b81bac4a55066c4edc5fe4d42722f54b7a9577
Ali Lopez <ali.lopez@accenture.com>
2024-08-31 13:07:02 -0300

    refactor: se simplifica para adaptarlo a un mvp

--- src/index.js
+++ src/index.js
@@ -1,42 +1,17 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const jwt = require('jsonwebtoken');
-const config = require('./helper/config/server');
-const app = config(express());
+const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const Environment = require('./helper/config/environment');
 
 const rooms = {};
-
-io.use((socket, next) => {
-    const eventName = socket.eventName;
-    if (eventName === 'create_room' || eventName === 'join_room') {
-        return next();
-    }
-
-    const token = socket.handshake.auth.token;
-    if (!token) {
-        return next(new Error('Authentication error'));
-    }
-
-    jwt.verify(token, Environment.JWT_SECRET_KEY, (err, decoded) => {
-        if (err) {
-            return next(new Error('Authentication error'));
-        }
-        socket.user = decoded;
-        next();
-    });
-});
 
 io.on('connection', (socket) => {
     console.log('New client connected');
 
     socket.on('create_room', ({ roomId, maxParticipants, name }) => {
         if (!rooms[roomId]) {
-            const adminPayload = { name, roomId };
-            const token = jwt.sign(adminPayload, JWT_SECRET, { expiresIn: '1h' });
             rooms[roomId] = {
                 admin: socket.id,
                 participants: {},
@@ -46,24 +21,21 @@ io.on('connection', (socket) => {
             };
             rooms[roomId].participants[socket.id] = { name: 'Admin', vote: null };
             socket.join(roomId);
-            socket.emit('room_created', { roomId, adminId: rooms[roomId].admin, token });
+            socket.emit('room_created', { roomId, adminId: rooms[roomId].admin });
             console.log(`Room ${roomId} created with admin ${socket.id}`);
         } else {
             socket.emit('error', 'Room already exists');
         }
     });
 
-    socket.on('join_room', ({ name }) => {
-        roomId = rooms.filter(room => room.name === name)[0].id;
+    socket.on('join_room', ({ name, roomId }) => {
         if (rooms[roomId]) {
             const room = rooms[roomId];
-            const participantPayload = { name, roomId };
-            const token = jwt.sign(participantPayload, JWT_SECRET, { expiresIn: '1h' });
             if (Object.keys(room.participants).length < room.maxParticipants) {
                 room.participants[socket.id] = { name: name, vote: null };
                 socket.join(roomId);
                 socket.emit('room_joined', { roomId, adminId: rooms[roomId].admin });
-                io.to(roomId).emit('user_joined', { userId: socket.id, name: name, token });
+                io.to(roomId).emit('user_joined', { userId: socket.id, name: name });
                 console.log(`${name} joined room ${roomId}`);
             } else {
                 socket.emit('error', 'Room is full');
@@ -76,7 +48,7 @@ io.on('connection', (socket) => {
     socket.on('vote', ({ roomId, vote }) => {
         if (rooms[roomId] && rooms[roomId].participants[socket.id]) {
             const room = rooms[roomId];
-            if (typeof vote === 'number' && vote >= 0) {
+            if (typeof vote === 'number' && [1, 2, 3, 5, 8, 13, 21].includes(vote)) {
                 room.participants[socket.id].vote = vote;
                 room.votes[socket.id] = vote;
                 room.lastActivity = Date.now();
@@ -97,9 +69,9 @@ io.on('connection', (socket) => {
     socket.on('reset_votes', ({ roomId }) => { 
         if (rooms[roomId] && rooms[roomId].admin === socket.id) {
             rooms[roomId].votes = {};
-            rooms[roomId].participants.forEach(participant => {
-                rooms[roomId].participants[participant].vote = null;
-            });
+            for (const participantId in rooms[roomId].participants) {
+                rooms[roomId].participants[participantId].vote = null;
+            }
             rooms[roomId].lastActivity = Date.now();
             io.to(roomId).emit('votes_reset');
             console.log(`Votes reset in room ${roomId}`);
@@ -108,28 +80,12 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('kick_user', ({ roomId, userId }) => {
-        if (rooms[roomId] && rooms[roomId].admin === socket.id) {
-            if (rooms[roomId].participants[userId]) {
-                delete rooms[roomId].participants[userId];
-                delete rooms[roomId].votes[userId];
-                io.to(userId).emit('kicked');
-                io.sockets.sockets.get(userId).leave(roomId);
-                console.log(`User ${userId} kicked from room ${roomId}`);
-            } else {
-                socket.emit('error', 'User not found in room');
-            }
-        } else {
-            socket.emit('error', 'Only admin can kick users or room does not exist');
-        }
-    });
-
     socket.on('close_room', ({ roomId }) => { 
         if (rooms[roomId] && rooms[roomId].admin === socket.id) {
             io.to(roomId).emit('room_closed');
-            rooms[roomId].participants.forEach(participant => {
-                io.sockets.sockets.get(participant.id)?.leave(roomId);
-            });
+            for (const participantId in rooms[roomId].participants) {
+                io.sockets.sockets.get(participantId)?.leave(roomId);
+            }
             delete rooms[roomId];
             console.log(`Room ${roomId} closed by admin`);
         } else {
@@ -145,13 +101,14 @@ io.on('connection', (socket) => {
         }
         const room = rooms[roomId];
         const isAdmin = room.admin === userId;
-        room.participants = room.participants.filter(participant => participant.id !== userId);
+        delete room.participants[userId];
+        delete room.votes[userId];
         io.sockets.sockets.get(userId)?.leave(roomId);
         if (isAdmin) {
-            io.to(roomId).emit('room_closed', 'El cuarto ha sido cerrado por el admin');
-            room.participants.forEach(participant => {
-                io.sockets.sockets.get(participant.id)?.leave(roomId);
-            });
+            io.to(roomId).emit('room_closed', 'Room closed by the admin');
+            for (const participantId in room.participants) {
+                io.sockets.sockets.get(participantId)?.leave(roomId);
+            }
             delete rooms[roomId];
         }
     });
@@ -169,6 +126,6 @@ io.on('connection', (socket) => {
     }, 60 * 1000); // check every minute
 });
 
-server.listen(Environment.PORT, () => {
-    console.log(`'Listening on port ${Environment.PORT}`);
+server.listen(3000, () => {
+    console.log(`Listening on port 3000`);
 });
